7c311a86259480abd1bcfebdeb961aeaf3864b52
dolezha <od10632@gmail.com>
2025-03-02 18:49:37 +0300

    v2

--- handlers/calculate_handler.go
+++ handlers/calculate_handler.go
@@ -16,7 +16,6 @@ func CalculateHandler(w http.ResponseWriter, r *http.Request) {
 
 	var reqBody models.RequestBody
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-<<<<<<< HEAD
 		utils.RespondWithJSON(w, map[string]string{"error": "Invalid request body"}, http.StatusUnprocessableEntity)
 		return
 	}
@@ -32,31 +31,6 @@ func CalculateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := utils.RespondWithJSON(w, response, http.StatusCreated); err != nil {
-=======
-		response := models.ResponseBody{
-			Error: utils.ToPtr("Invalid request body"),
-		}
-		utils.RespondWithJSON(w, response, http.StatusUnprocessableEntity)
-		return
-	}
-
-	result, err := services.Calc(reqBody.Expression)
-	if err != nil {
-		response := models.ResponseBody{
-			Error: utils.ToPtr("Expression is not valid"),
-		}
-		if err.Error() == "деление на ноль" {
-			response.Error = utils.ToPtr("Division by zero")
-		}
-		utils.RespondWithJSON(w, response, http.StatusUnprocessableEntity)
-		return
-	}
-
-	response := models.ResponseBody{
-		Result: &result,
-	}
-	if err := utils.RespondWithJSON(w, response, http.StatusOK); err != nil {
->>>>>>> c030495b399869c8d46fd81d0464189aaf4466c3
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
